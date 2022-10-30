551f3b1252abcdd1651f31eef48126c98d4380f7
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2022-10-30 10:50:18 -0700

    dfssdf

---
+++ assets/gamepage/js/OVR.js
@@ -0,0 +1,52 @@
+let voteable;
+
+function Qatar() {
+  voteable = "1 Qatar win the match";
+  document.getElementById("Qatar").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ecuador() {
+  voteable = "1 Ecuador win the match";
+  document.getElementById("Ecuador").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function England() {
+  voteable = "1 England win the match";
+  document.getElementById("England").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function IR() {
+  voteable = "1 IR win the match";
+  document.getElementById("IR").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Senegal() {
+  voteable = "1 Senegal win the match";
+  document.getElementById("Senegal").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Netherlands() {
+  voteable = "1 Netherlands win the match";
+  document.getElementById("Netherlands").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function USA() {
+  voteable = "1 USA win the match";
+  document.getElementById("USA").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Wales() {
+  voteable = "1 Wales win the match";
+  document.getElementById("Wales").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Argentina() {
+  voteable = "1 Argentina win the match";
+  document.getElementById("Argentina").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Arabia() {
+  voteable = "1 Arabia win the match";
+  document.getElementById("Arabia").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
