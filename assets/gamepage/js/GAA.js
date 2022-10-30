44f63112cbeab8358dc1707556534ce02cea8d9c
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2022-10-30 08:13:04 -0700

    asdsad

---
+++ assets/gamepage/js/GAA.js
@@ -0,0 +1,62 @@
+let voteable;
+
+function Qatar() {
+  voteable = "1 Qatar win the match";
+  document.getElementById("Qatar").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ecuador() {
+  voteable = "1 Ecuador win the match";
+  document.getElementById("Ecuador").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Senegal() {
+  voteable = "1 Senegal win the match";
+  document.getElementById("Senegal").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Netherlands() {
+  voteable = "1 Netherlands win the match";
+  document.getElementById("Netherlands").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Qata() {
+  voteable = "1 Qatar win the match";
+  document.getElementById("Qata").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Senega() {
+  voteable = "1 Senegal win the match";
+  document.getElementById("Senega").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Netherland() {
+  voteable = "1 Netherlands win the match";
+  document.getElementById("Netherland").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ecuad() {
+  voteable = "1 Ecuador win the match";
+  document.getElementById("Ecuad").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ecua() {
+  voteable = "1 Ecuador win the match";
+  document.getElementById("Ecua").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Sene() {
+  voteable = "1 Senegal win the match";
+  document.getElementById("Sene").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Netherla() {
+  voteable = "1 Netherlands win the match";
+  document.getElementById("Netherla").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Qa() {
+  voteable = "1 Qatar win the match";
+  document.getElementById("Qa").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
