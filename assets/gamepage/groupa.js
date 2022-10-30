79ec9ec0fe2c5412d892bacdfff61e9d9bb120d8
Eissanoor <86971746+Eissanoor@users.noreply.github.com>
2022-10-30 08:00:26 -0700

    sadasdewrer

--- assets/gamepage/groupa.js
+++ assets/gamepage/groupa.js
@@ -1,20 +1,507 @@
 let voteable;
 
-function myFunction() {
-  const ran = Math.floor(Math.random() * 3) + 1;
-  if (ran == 1) {
-    voteable = "1 qatar win the match";
-    document.getElementById("qatar").innerHTML = voteable;
-    document.getElementById("btt").disabled = true;
-  } else if (ran == 3) {
-    voteable = "3 Ecuador win the match";
-    document.getElementById("Ecuador").innerHTML = voteable;
-    document.getElementById("btt").disabled = true;
-  } else if (ran == 2) {
-    voteable = "2 draw";
-    document.getElementById("draw").innerHTML = voteable;
-    document.getElementById("btt").disabled = true;
-  } else {
-    voteable = "Input is not a number";
-  }
+function qatar() {
+  voteable = "1 qatar win the match";
+  document.getElementById("qatar").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ecuador() {
+  voteable = "3 Ecuador win the match";
+  document.getElementById("Ecuador").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+
+  voteable = "Input is not a number";
+}
+
+function england() {
+  voteable = "1 england win the match";
+  document.getElementById("england").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Iran() {
+  voteable = "3 iran win the match";
+  document.getElementById("iran").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Senegal() {
+  voteable = "1 Senegal win the match";
+  document.getElementById("Senegal").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Netherlands() {
+  voteable = "3 Netherlands win the match";
+  document.getElementById("Netherlands").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function usa() {
+  voteable = "1 USA win the match";
+  document.getElementById("usa").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Wales() {
+  voteable = "3 Wales win the match";
+  document.getElementById("Wales").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Argentina() {
+  voteable = "1 Argentina win the match";
+  document.getElementById("Argentina").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Saudi() {
+  voteable = "3 Saudi arab win the match";
+  document.getElementById("Saudi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Denmark() {
+  voteable = "1 Denmark win the match";
+  document.getElementById("Denmark").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Tunisia() {
+  voteable = "3 Tunisia win the match";
+  document.getElementById("Tunisia").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Mexico() {
+  voteable = "1 Mexico win the match";
+  document.getElementById("Mexico").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Poland() {
+  voteable = "3 Poland win the match";
+  document.getElementById("Poland").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function France() {
+  voteable = "1 France win the match";
+  document.getElementById("France").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Australia() {
+  voteable = "3 Australia win the match";
+  document.getElementById("Australia").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Morocco() {
+  voteable = "1 Morocco win the match";
+  document.getElementById("Morocco").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Croatia() {
+  voteable = "3 Croatia win the match";
+  document.getElementById("Croatia").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Germany() {
+  voteable = "1 Germany win the match";
+  document.getElementById("Germany").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Japan() {
+  voteable = "3 Japan win the match";
+  document.getElementById("Japan").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Spain() {
+  voteable = "1 Spain win the match";
+  document.getElementById("Spain").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Costa() {
+  voteable = "3 Costa win the match";
+  document.getElementById("Costa").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Belgium() {
+  voteable = "1 Belgium win the match";
+  document.getElementById("Belgium").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Canada() {
+  voteable = "3 Canada win the match";
+  document.getElementById("Canada").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Switzerland() {
+  voteable = "1 Switzerland win the match";
+  document.getElementById("Switzerland").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Cameroon() {
+  voteable = "3 Cameroon win the match";
+  document.getElementById("Cameroon").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Uruguay() {
+  voteable = "1 Uruguay win the match";
+  document.getElementById("Uruguay").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Korea() {
+  voteable = "3 Korea Republic win the match";
+  document.getElementById("Korea").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Portugal() {
+  voteable = "1 Portugal Republic win the match";
+  document.getElementById("Portugal").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Ghana() {
+  voteable = "3 Ghana Republic win the match";
+  document.getElementById("Ghana").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Brazil() {
+  voteable = "1 Brazil Republic win the match";
+  document.getElementById("Brazil").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Serbia() {
+  voteable = "3 Serbia Republic win the match";
+  document.getElementById("Serbia").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Wale() {
+  voteable = "3 Wales win the match";
+  document.getElementById("Wale").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Iran() {
+  voteable = "3 Iran win the match";
+  document.getElementById("Iran").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function qata() {
+  voteable = "3 qatar win the match";
+  document.getElementById("qata").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Senega() {
+  voteable = "3 Senegal win the match";
+  document.getElementById("Senega").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Netherland() {
+  voteable = "3 Netherlands win the match";
+  document.getElementById("Netherland").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ecuado() {
+  voteable = "1 Ecuador win the match";
+  document.getElementById("Ecuado").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Englan() {
+  voteable = "1 England win the match";
+  document.getElementById("Englan").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function USA() {
+  voteable = "1 USA win the match";
+  document.getElementById("USA").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Tunisi() {
+  voteable = "1 Tunisia win the match";
+  document.getElementById("Tunisi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Australi() {
+  voteable = "1 Australia win the match";
+  document.getElementById("Australi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Polan() {
+  voteable = "1 Poland win the match";
+  document.getElementById("Polan").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Saud() {
+  voteable = "1 Saudi Arabia win the match";
+  document.getElementById("Saud").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Franc() {
+  voteable = "1 France win the match";
+  document.getElementById("Franc").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Denmar() {
+  voteable = "1 Denmark win the match";
+  document.getElementById("Denmar").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Argentin() {
+  voteable = "1 Argentina win the match";
+  document.getElementById("Argentin").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Mexic() {
+  voteable = "1 Mexico win the match";
+  document.getElementById("Mexic").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Japa() {
+  voteable = "1 Japan win the match";
+  document.getElementById("Japa").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Rica() {
+  voteable = "1 Costa Rica win the match";
+  document.getElementById("Rica").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Belgiu() {
+  voteable = "1 Belgium win the match";
+  document.getElementById("Belgiu").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Morocc() {
+  voteable = "1 Morocco win the match";
+  document.getElementById("Morocc").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Croati() {
+  voteable = "1 Croatia win the match";
+  document.getElementById("Croati").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Canad() {
+  voteable = "1 Canada win the match";
+  document.getElementById("Canad").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Spai() {
+  voteable = "1 Spain win the match";
+  document.getElementById("Spai").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function German() {
+  voteable = "1 Germany win the match";
+  document.getElementById("German").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Cameroo() {
+  voteable = "1 Cameroon win the match";
+  document.getElementById("Cameroo").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Serbi() {
+  voteable = "1 Serbia win the match";
+  document.getElementById("Serbi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Republic() {
+  voteable = "1 Korea Republic win the match";
+  document.getElementById("Republic").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ghan() {
+  voteable = "1 Ghana win the match";
+  document.getElementById("Ghan").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Brazi() {
+  voteable = "1 Brazil win the match";
+  document.getElementById("Brazi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Switzerlan() {
+  voteable = "1 Switzerlan win the match";
+  document.getElementById("Switzerlan").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Portuga() {
+  voteable = "1 Portugal win the match";
+  document.getElementById("Portuga").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Urugua() {
+  voteable = "1 Uruguay win the match";
+  document.getElementById("Urugua").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Ecuad() {
+  voteable = "1 Ecuador win the match";
+  document.getElementById("Ecuad").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Seneg() {
+  voteable = "1 Senegal win the match";
+  document.getElementById("Seneg").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Netherlan() {
+  voteable = "1 Netherlands win the match";
+  document.getElementById("Netherlan").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Qat() {
+  voteable = "1 Qatar win the match";
+  document.getElementById("Qat").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Wal() {
+  voteable = "1 Wales win the match";
+  document.getElementById("Wal").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Engla() {
+  voteable = "1 England win the match";
+  document.getElementById("Engla").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function IR() {
+  voteable = "1 IR Iran win the match";
+  document.getElementById("IR").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function U() {
+  voteable = "1 USA win the match";
+  document.getElementById("U").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Denma() {
+  voteable = "1 Denmark win the match";
+  document.getElementById("Denma").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Austral() {
+  voteable = "1 Australia win the match";
+  document.getElementById("Austral").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Tunis() {
+  voteable = "1 Tunisia win the match";
+  document.getElementById("Tunis").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Fran() {
+  voteable = "1 France win the match";
+  document.getElementById("Fran").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Pola() {
+  voteable = "1 Poland win the match";
+  document.getElementById("Pola").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Argent() {
+  voteable = "1 Argentina win the match";
+  document.getElementById("Argent").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Arabi() {
+  voteable = "1 sudia Arabia win the match";
+  document.getElementById("Arabi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Mexi() {
+  voteable = "1 Mexico win the match";
+  document.getElementById("Mexi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Croat() {
+  voteable = "1 Croatia win the match";
+  document.getElementById("Croat").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Belgi() {
+  voteable = "1 Belgium win the match";
+  document.getElementById("Belgi").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Cana() {
+  voteable = "1 Canada win the match";
+  document.getElementById("Cana").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Moro() {
+  voteable = "1 Morocco win the match";
+  document.getElementById("Moro").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Jap() {
+  voteable = "1 Japan win the match";
+  document.getElementById("Jap").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Spa() {
+  voteable = "1 Spain win the match";
+  document.getElementById("Spa").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Cos() {
+  voteable = "1 Costa rica win the match";
+  document.getElementById("Cos").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Germa() {
+  voteable = "1 Germany win the match";
+  document.getElementById("Germa").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Gha() {
+  voteable = "1 Ghana win the match";
+  document.getElementById("Gha").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Urugu() {
+  voteable = "1 Uruguay win the match";
+  document.getElementById("Urugu").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+
+function Republ() {
+  voteable = "1 Korea Republic win the match";
+  document.getElementById("Republ").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Portug() {
+  voteable = "1 Portugal win the match";
+  document.getElementById("Portug").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Serb() {
+  voteable = "1 Serbia win the match";
+  document.getElementById("Serb").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Switzerla() {
+  voteable = "1 Switzerland win the match";
+  document.getElementById("Switzerla").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Camero() {
+  voteable = "1 Cameroon win the match";
+  document.getElementById("Camero").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
+}
+function Bra() {
+  voteable = "1 Brazil win the match";
+  document.getElementById("Bra").innerHTML = voteable;
+  document.getElementById("btt").disabled = true;
 }
